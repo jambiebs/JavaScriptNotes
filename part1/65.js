6d8cd24a46f5fa7db0098bdba3f5c7fb05e710eb
Jam_Al <jambiebs@gmail.com>
2022-10-18 20:28:53 +0530

    continue...

--- part1/65.js
+++ part1/65.js
@@ -15,7 +15,20 @@
 // console.log(myArray);
 
 
-const myArray1 = ["item1", "item2", "item3"];
-const jamal = myArray1.splice(1,1,"jamal");
-console.log(jamal);  // this will give the output of deleted/added items
-console.log(myArray1); // this is your main array that has changed 
+// const myArray1 = ["item1", "item2", "item3"];
+// const jamal = myArray1.splice(1,1,"jamal"); // splice is used to add or delete the items from array, it works indexwise 
+// console.log(jamal);  // this will give the output of deleted/added items
+// console.log(myArray1); // this is your main array that has changed 
+
+
+
+
+// practice
+// splice method changes the main array
+
+const itemList1 = ["a", "b", "c", "d"];
+const remove = itemList1.splice(2,1); // here first index is from where you want to start, and second index is how many item you want to delete
+console.log(remove);
+const add = itemList1.splice(2,1, "c", "d");
+console.log(add);
+console.log(itemList1);
