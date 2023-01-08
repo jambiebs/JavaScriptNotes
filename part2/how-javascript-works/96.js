cec3d167f39948ecddd38c5400a0e6cfb43aaa94
Jam_Al <jambiebs@gmail.com>
2023-01-08 12:17:23 +0530

    ECMA Script folder and pdf file added

--- part2/how-javascript-works/96.js
+++ part2/how-javascript-works/96.js
@@ -1,14 +1,17 @@
-// function execution context 
+// // function execution context 
 
-let foo = "foo";
-console.log(foo);
-function getFullName(firstName, lastName){
-    console.log(arguments);
-    let myVar = "var inside func";
-    console.log(myVar);
-    const fullName = firstName + " " + lastName;
-    return fullName;
-}
+// let foo = "foo";
+// console.log(foo);
+// function getFullName(firstName, lastName){
+//     console.log(arguments);
+//     let myVar = "var inside func";
+//     console.log(myVar);
+//     const fullName = firstName + " " + lastName;
+//     return fullName;
+// }
 
-const personName = getFullName("harshit", "sharma");
-console.log(personName);
+// const personName = getFullName("harshit", "sharma");
+// console.log(personName);
+
+
+console.log(arguments);
--- part2/how-javascript-works/hjw_01.js
+++ part2/how-javascript-works/hjw_01.js
@@ -15,3 +15,18 @@ console.log(window);
 console.log(firstName);
 var firstName = "Harshit";
 console.log(firstName);
+
+
+// detailed Descriptions; 
+// there are two phase of compilation 
+// 1. compile
+// 2. code execute
+// why to compile? why can't we execute line by line.
+// ther are theree phase of compilation 
+// 1. tokenizing/lexing
+// 2. parsing
+// 3. code excution 
+// in first phase compiler break the code into chunks; that is called tokenizing; it create 'abstract syntax tree(AST)'
+// in parsing phase javascript decide the variable scope and syntex format is correct or not;   
+// official ecma script says that it is not necessary to compile the code but there should be 1. early error checking 2. determining appropriate scope for variable; 
+// 
